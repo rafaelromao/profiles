39cfb82cd813457385f4bc83c5c1d6338a8761d1
Rafael Romão <rafaelromao@gmail.com>
2020-07-15 10:42:40 -0300

    unify powershell and oh-my-posh

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+# Install-Module posh-git -Scope CurrentUser
+# Install-Module oh-my-posh -Scope CurrentUser
+# Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme ParadoxRomao
+Set-PSReadlineOption -BellStyle None
+clear
